56b7c179e3b4db6dc9c2b1f344ddfab0c219e70c
Illia Kocherovets <illiakocherovets@gmail.com>
2025-02-10 01:52:33 +0100

    push rest

--- backend/event_booking_backend/property/admin.py
+++ backend/event_booking_backend/property/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Property
+from .models import Property, Reservation, User
 
 admin.site.register(Property)
+admin.site.register(Reservation)
+admin.site.register(User)
--- backend/event_booking_backend/property/serializers.py
+++ backend/event_booking_backend/property/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Property
+from .models import Property, Reservation
 from useraccount.serializers import UserDetailSerializer
 
 class PropertiesListSerializer(serializers.ModelSerializer):
@@ -28,4 +28,13 @@ class PropertiesDetailSerializer(serializers.ModelSerializer):
             'bathrooms',
             'guests',
             'landlord'
+        )
+
+class ReservationsListSerializer(serializers.ModelSerializer):
+    property = PropertiesListSerializer(read_only=True, many=False)
+    
+    class Meta:
+        model = Reservation
+        fields = (
+            'id', 'start_date', 'end_date', 'number_of_nights', 'total_price', 'property'
         )
